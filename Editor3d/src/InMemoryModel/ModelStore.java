23d53861f8853a32908c3bc1795ded3ff23a54f4
Golikov-Andrey <newsc2@yandex.ru>
2023-09-29 22:59:21 +0300

    solution from seminar2 architecture software

--- Editor3d/src/InMemoryModel/ModelStore.java
+++ Editor3d/src/InMemoryModel/ModelStore.java
@@ -7,6 +7,7 @@ import ModelEliments.Camera;
 import ModelEliments.Flash;
 import ModelEliments.PoligonalModel;
 import ModelEliments.Scene;
+import ModelEliments.Texture;
 
 public class ModelStore implements iModelChanger {
     public List<PoligonalModel> models;
@@ -15,13 +16,19 @@ public class ModelStore implements iModelChanger {
     public List<Camera> cameras;
     private iModelChangeObserver[] changeObservers;
 
-    public ModelStore(iModelChangeObserver[] changeObservers) {
+    public ModelStore(iModelChangeObserver[] changeObservers) throws Exception {
         this.changeObservers = changeObservers;
 
         this.models = new ArrayList<PoligonalModel>();
         this.scenes = new ArrayList<Scene>();
         this.flashes = new ArrayList<Flash>();
         this.cameras = new ArrayList<Camera>();
+
+        List<Texture> Textures = new ArrayList<>();
+        models.add(new PoligonalModel(Textures));
+        flashes.add(new Flash());
+        cameras.add(new Camera());
+        scenes.add(new Scene(0, models, flashes, cameras));
     }
 
     public Scene getScena(int id)
